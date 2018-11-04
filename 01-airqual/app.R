785e22ccaf2d4096e6d4cc1750f076b275145f9c
Alex <achauvinhameau@gmail.com>
2018-11-04 15:38:42 +0100

    first version

---
+++ 01-airqual/app.R
@@ -0,0 +1,48 @@
+library(shiny)
+require(graphics)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Air Quality in NYC - 1973"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         sliderInput("month",
+                     "Month:",
+                     min = 5,
+                     max = 9,
+                     value = c(5,9))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("pairs"),
+         tableOutput("values")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$pairs <- renderPlot({
+     # suppress days from pairs graph
+     d <- airquality[,1:5]
+     d <- d[d$Month >= input$month[1] & d$Month <= input$month[2], ]
+     pairs(d, panel = panel.smooth, main = "airquality data")
+   })
+   
+   output$values <- renderTable({
+     d <- airquality
+     d <- d[d$Month >= input$month[1] & d$Month <= input$month[2], ]
+     
+     head(d[order(d$Month, d$Day),], n=10)
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # r-labs
+
+lab 1: airqual - simple dataviz based on NYC air quality datas (integrated in R)
